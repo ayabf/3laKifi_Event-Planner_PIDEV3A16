a7381a8db6a37320220e90e150f2cfed114fd3a5
nour badreddine <nourbadreddine999@gmail.com>
2025-02-16 20:49:42 +0100

    database connexion

--- src/main/java/tests/main.java
+++ src/main/java/tests/main.java
@@ -1,9 +1,13 @@
 package tests;
 
 import java.sql.SQLException;
+import utils.DataSource;
 
 public class main {
     public static void main(String[] args) {
+        DataSource.getInstance();
+
+
     }
     }
 
