2ee20f1da393a6b84aecd5430feb760160a69b6a
Dheker-Laadhibi <dhekerlaadhibii@gmail.com>
2025-02-16 13:53:43 +0100

    squelette

---
+++ src/main/java/tests/main.java
@@ -0,0 +1,10 @@
+package tests;
+
+import java.sql.SQLException;
+
+public class main {
+    public static void main(String[] args) {
+    }
+    }
+
+
---
+++ src/main/java/tests/mainFx.java
@@ -0,0 +1,21 @@
+package tests;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class mainFx extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
